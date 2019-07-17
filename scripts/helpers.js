5387ab0d52f2b81f84cd3efea1e716f2b05b66b4
andytudhope <tuddy0525@gmail.com>
2019-07-17 12:01:33 +0200

    Final checks

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -176,23 +176,3 @@ hexo.extend.helper.register('disqus_lang', function() {
 hexo.extend.helper.register('hexo_version', function() {
   return this.env.version;
 });
-
-function generateMenu(){
-  return fetch('https://raw.githubusercontent.com/status-im/status-global-elements/master/dist/html/header.html')
-  .then(function(response) {
-      return response.text();
-    })
-  .then(function(response) {
-      console.log('t2')
-      return 'abc';
-  })
-  .catch(error => console.error(`Fetch Error =\n`, error));
-}
-
-hexo.extend.helper.register('global_header', function() {
-  generateMenu().then(function(response){
-    console.log(response);
-    return response;
-  });
-  return 'asd';
-});
